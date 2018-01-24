440880f24daa0de1392df6d8256a703ab504ab92
Takumi Adachi <takumiadachi@gmail.com>
2018-01-24 01:12:27 -0800

    Added fallback image for twitch user logo.

--- src/components/UserPaper.js
+++ src/components/UserPaper.js
@@ -16,12 +16,22 @@ const logo = {
 
 class UserPaper extends React.Component {
 
+
   render() {
+    let onError = () => {
+      this.refs.user_logo.src = 'http://is2.mzstatic.com/image/thumb/Purple128/v4/f1/b1/7e/f1b17eb1-6589-15c8-7f9c-9c3c0d35e086/source/175x175bb.jpg'
+    }
+
     return (
       <Paper style={style}>
-        <div><img style={logo} 
-          alt='http://is2.mzstatic.com/image/thumb/Purple128/v4/f1/b1/7e/f1b17eb1-6589-15c8-7f9c-9c3c0d35e086/source/175x175bb.jpg' 
-          src={this.props.img} /></div>
+        <div>
+          <img style={logo}
+            onError={onError}
+            alt='User Logo'
+            src={this.props.img}
+            ref='user_logo'
+            />
+        </div>
         <div>{this.props.name}</div>
         {this.props.children}
       </Paper>
